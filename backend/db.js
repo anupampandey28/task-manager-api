c03c189e7c9b07a5bfdb512a74f327dc10d675e8
anupampandey28 <anupam.pandey2811@gmail.com>
2026-02-22 18:43:19 +0530

    refractor: separate routes , add validation and environment variables

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,15 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
 import pg from 'pg';
+
 const { Pool } = pg;
 const pool = new Pool({
-    host: 'localhost',
-    port: 5432,
-    database: 'habittracker',
-    user: 'anupam',
-    password:null,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
 });
 
 export default pool;
--- backend/server.js
+++ backend/server.js
@@ -1,56 +1,12 @@
 import express from 'express';
-import  pool  from './db.js'
+import habitRoutes from './routes/habits.route.js'
 
 const app = express();
 app.use(express.json());
 
 
-app.get('/' ,(req,res) =>{
-    res.send("Habit Tracker Server is running")
-})
-
-//Routes
-app.post('/habits' , async(req , res)=>{
-    try {
-        const name = req.body;
-        const result = await pool.query('INSERT INTO habits (name) VALUES ($1) RETURNING *' , [name]);
-        res.status(201).json(result.rows[0])       
-    } catch (error) {
-    console.error(error);
-    res.status(500).send('Database connection failed');  
-    }
-})
-
-app.get('/habits' , async(req , res) => {
-    try {
-        const result = await pool.query('SELECT * FROM habits')
-        res.status(200).json(result.rows  )
-    } catch (error) {
-        res.status(500).json({error:error.message})
-    }
-})
-
-app.put('/habits/id',async(req , res) =>{
-    try {
-        const {id} = req.params;
-        const {name} = req.body;
-        const result = await pool.query('UPDATE habits SET name = $1 WHERE id =$2 RETURNING *', [name , id])
-        res.status(200).json(result.rows[0]);
-    } catch (error) {
-        res.status(500).json({error:error.message})
-    }
-})
-
-app.delete('/habits/:id' , async(req , res)=>{
-try {
-    const {id }= req.params;
-    const result = await pool.query('DELETE FROM habits WHERE id = $1',[id])
-    res.status(200).json({message : "Habit deleted succesfully"})
-} catch (error) {
-    res.status(500).json({error:error.message})
-}
-})
-
+app.use('/habits' , habitRoutes)
+  
 app.listen(3000 , () =>{
     console.log("Server is running on port 3000");
 })
