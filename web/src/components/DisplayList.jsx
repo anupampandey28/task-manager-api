ebe267fa102ce552267cf19bc1105be975c94036
anupampandey28 <anupam.pandey2811@gmail.com>
2026-03-12 17:27:27 +0530

    trigger vercel redeploy

--- web/src/components/DisplayList.jsx
+++ web/src/components/DisplayList.jsx
@@ -8,7 +8,8 @@ const DisplayList = () => {
   const [currentTime, setCurrentTime] = useState(dayjs());
   const [isCreateOpen, setIsCreateOpen] = useState(false);
   const [isClosing, setIsClosing] = useState(false);
-  const API = import.meta.env.VITE_API_URL;
+ const API =
+  import.meta.env.VITE_API_URL || "https://taskmanager1070.onrender.com";
 
   
 
